8c89d4b3f42e8785bd119fdd094008792ff9e5b7
deainya <deainya@gmail.com>
2021-12-12 11:50:09 +0300

    fix

--- htdocs/app.js
+++ htdocs/app.js
@@ -320,7 +320,7 @@ bot.catch((err) => {
     console.log('Doh!\n', err);
     //Trying to handle "Too Many Requests..."
     if (err.code == 429 && err.on.method == 'sendPhoto') {
-        setTimeout(bot.telegram.sendPhoto(err.on.payload.chat_id, err.on.payload.photo, 3000);
+        setTimeout(bot.telegram.sendPhoto(err.on.payload.chat_id, err.on.payload.photo), 3000);
     }
     if (err.code == 429 && err.on.method == 'sendMessage') {
         setTimeout(bot.telegram.sendMessage(err.on.payload.chat_id, err.on.payload.text/*, { parse_mode: "MarkdownV2" }*/), 3000);
