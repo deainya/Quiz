3be8d8963561872f17f1bbf2e668fe2d48c4355a
deainya <deainya@gmail.com>
2021-12-12 12:08:42 +0300

    try

--- htdocs/app.js
+++ htdocs/app.js
@@ -322,13 +322,13 @@ bot.on('photo', async (ctx) => {
 bot.catch((err) => {
     console.log('Doh!\n', err);
     //Trying to handle "Too Many Requests..."
-    if (err.code == 429 && err.on.method == 'sendPhoto') {
-        //setTimeout(bot.telegram.sendPhoto(err.on.payload.chat_id, err.on.payload.photo), 3000);
-        setTimeout(bot.telegram.sendMessage(err.on.payload.chat_id, err.on.payload.photo/*, { parse_mode: "MarkdownV2" }*/), 3000);
-    }
     if (err.code == 429 && err.on.method == 'sendMessage') {
         setTimeout(bot.telegram.sendMessage(err.on.payload.chat_id, err.on.payload.text/*, { parse_mode: "MarkdownV2" }*/), 3000);
     }
+    //if (err.code == 429 && err.on.method == 'sendPhoto') {
+        //setTimeout(bot.telegram.sendPhoto(err.on.payload.chat_id, err.on.payload.photo), 3000);
+        //setTimeout(bot.telegram.sendMessage(err.on.payload.chat_id, err.on.payload.photo/*, { parse_mode: "MarkdownV2" }*/), 3000);
+    //}
 })
 
 // Запускаем poll обработчик бота
