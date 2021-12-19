9072b298cec00ed73997e64bc8f51b1cb777068f
deainya <deainya@gmail.com>
2021-12-19 20:48:56 +0300

    fix

--- htdocs/app.js
+++ htdocs/app.js
@@ -149,10 +149,10 @@ bot.command('scoreit', async (ctx) => {
         score.push({id: qRs[i].chat, title: qRs[i].title, t: qRs[i].total});
         str = str + qRs[i].title + ' (' + qRs[i].chat.toString() + ')\n'
               + 'try: ' + qRs[i].trys.toString() + '\n'
-              + 'pts: ' + qRs[i].pts.toString() + '\n';
-        time = time + qRs[i].chat.toString() + ')\n'
+              + 'pts: ' + qRs[i].pts.toString() + '\n\n';
+        time = time + qRs[i].chat.toString() + '\n'
               + 't1: ' + qRs[i].t1.toString() + '\n'
-              + 't2: ' + qRs[i].t2.toString() + '\n';
+              + 't2: ' + qRs[i].t2.toString() + '\n\n';
     }
     var msg = '';
     //console.log(qRs);
